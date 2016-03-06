bc90994971027a67dcc58815b30b3e43e728dbe4
Chester Holtz <chesterholtz@gmail.com>
2016-03-06 00:08:06 -0500

    updated help fnc

--- count_errors.py
+++ count_errors.py
@@ -8,18 +8,19 @@ import ATD
 ATD.setDefaultKey("warner_findoc_research")
 from enchant.checker import *
 
-def enum_errs(cleaned_text):
+def enum_errs(cleaned_text,g):
     f = open(cleaned_text)
     f_str = f.read()
     f.seek(0)
     pwl = open("custom_dict.txt")
     chkr = SpellChecker("en_US")
+    tot_sp = 0 # spelling errors
+    tot_gm = -1 # grammatical errors -1 if no checking specified
 
     for w in pwl:
         chkr.add(w)
 
     text = ""
-    tot_sp = 0
     for line in f:
         for word in line.split():
             if re.search('[a-zA-Z]', word) and len(word) > 1 and word.find(".com") == -1:
@@ -28,19 +29,20 @@ def enum_errs(cleaned_text):
     chkr.set_text(text)
     for err in chkr:
         if err.word[0].isupper() == False:
-            #print "spelling error for: " + "**"+err.word+"**"
+            with open(cleaned_text + ".log", "w") as f:
+                f.write("spelling error for: " + "**"+err.word+"**\n")
             tot_sp += 1
     #print "#Total spelling errors for file: " + str(cleaned_text) + ": " + str(tot_sp)
-
-    tot_gm = 0
-    for sentence in re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', f_str):
-        time.sleep(1)
-        errors = ATD.checkGrammar(sentence)
-        for err in errors:
-            if err.type == "grammar":
-                print "%s error for: %s **%s**" % (err.type, err.precontext, err.string)
-                tot_gm += 1
-    #print "#Total grammatical errors for file: " + sys.argv[1] + ": " + str(tot_gm)
+    if g == "-g":
+        for sentence in re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', f_str):
+            time.sleep(1)
+            errors = ATD.checkGrammar(sentence)
+            for err in errors:
+                if err.type == "grammar":
+                    with open(cleaned_text + ".log", "a+") as f:
+                        f.write("%s error for: %s **%s**\n" % (err.type, err.precontext, err.string))
+                    tot_gm += 1
+        #print "#Total grammatical errors for file: " + sys.argv[1] + ": " + str(tot_gm)
 
     f.close()
     return tot_sp, tot_gm
--- wraper.py
+++ wraper.py
@@ -4,27 +4,42 @@ import os
 import csv
 import parse_lib
 import count_errors
+def usage():
+    print "usage: python wraper.py <data/>{mandatory} <-c cores>{optional} <-g>{optional}"
+
 def main():
+    if len(sys.argv) < 2:
+        usage()
+        sys.exit(1)
     cleaned_text = ""
-    errs = ""
     baseline = ()
-    for report in os.listdir("."):
+    data_dir = sys.argv[1]
+    g = sys.argv[2] if len(sys.argv)>3 else ""
+    if not os.path.exists("result"):
+        os.makedirs("result")
+
+    with open("result/" + "log" + ".csv",'w') as f:
+        csv_out = csv.writer(f)
+        csv_out.writerow(["fname","sic","filing type", "filing date", "date", "cname", "length", "sp errors", "gm errors"])
+    for report in os.listdir(data_dir):
+        file_dir = data_dir + "/" + report
         if report == "error_log.txt" or report == "custom_dict.txt" or report == "sp_test.txt":
             continue;
         if report.endswith(".txt"):
-            baseline = parse_lib.parse_file_meta(report)
+            baseline = parse_lib.parse_file_meta(file_dir)
             #print baseline
 
-            with open("cleaned_files/" + os.path.splitext(report)[0] + "_processed" + ".txt",'w') as f:
+            with open("result/" + os.path.splitext(report)[0] + "_processed" + ".txt",'w') as f:
                 print "cleaning text..."
-                cmd = "python html2text.py -b 0 " + report
+                cmd = "python html2text.py -b 0 " + file_dir
                 f.write(subprocess.check_output(cmd, shell=True))
             print "counting errors..."
-            sp_cnt,gm_cnt = str(count_errors.enum_errs("cleaned_files/" + os.path.splitext(report)[0] + "_processed" + ".txt"))
-            baseline = baseline + (errs,gm_cnt,)
+            sp_cnt,gm_cnt = count_errors.enum_errs("result/" + os.path.splitext(report)[0] + "_processed" + ".txt",g)
+            baseline = baseline + (sp_cnt,gm_cnt,)
             print baseline
-        with open("cleaned_files/" + os.path.splitext(report)[0] + "_log" + ".txt",'w') as f:
-            f.write(str(baseline))
+        with open("result/" + "log" + ".csv",'a+') as f:
+            csv_out = csv.writer(f)
+            csv_out.writerow(baseline)
 
 if __name__ == "__main__":
     main()
