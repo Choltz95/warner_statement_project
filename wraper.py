0cb0380d48684e9541d99c192e077af9d5487fb2
Chester Holtz <chesterholtz@gmail.com>
2016-03-24 01:47:24 -0400

    update

--- wraper.py
+++ wraper.py
@@ -4,6 +4,7 @@ import os
 import csv
 import parse_lib
 import count_errors
+from sets import set
 
 """
 data: directory containing forms
@@ -18,6 +19,7 @@ def argparse(argv):
     if len(sys.argv) < 2:
         usage()
         sys.exit(1)
+    #data_dir = r'/run/media/choltz2/My Passport/GJZ3'
     data_dir = sys.argv[1]
     g = "-g" if "-g" in sys.argv else ""
     uc = "-uc" if "-uc" in sys.argv else ""
@@ -34,8 +36,13 @@ def main():
 
     with open("result/" + "log" + ".csv",'w') as f:
         csv_out = csv.writer(f)
-        csv_out.writerow(["fname","sic","filing type", "filing date", "date", "cname", "length", "sp errors", "gm errors"])
+        if os.stat(f.name).st_size == 0:
+            csv_out.writerow(["fname","sic","filing type", "filing date", "date", "cname", "length", "sp errors", "gm errors"])
+    with open("file_log", "r") as f_log:
+        # do stuff
+        for line in f_log:
 
+        print "f_log open"
     for report in os.listdir(data_dir):
         file_dir = data_dir + "/" + report
         # ignore some temporary text files
@@ -43,7 +50,8 @@ def main():
             continue;
         if report.endswith(".txt"):
             baseline = parse_lib.parse_file_meta(file_dir)
-
+            with open("file_log",'a+') as f_log:
+                f_log.write(baseline[0] + "\n")
             with open("result/" + os.path.splitext(report)[0] + "_processed" + ".txt",'w') as f:
                 print "cleaning text..."
                 cmd = "python html2text.py -b 0 " + file_dir
